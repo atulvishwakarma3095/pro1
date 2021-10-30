dc84cbd3d16b65ce30132ab8fc33e16c85245051
atulvishwakarma3095 <atulvishwakarma3095@gmail.com>
2021-10-30 17:09:08 +0530

    Registor & Login page

---
+++ app/Views/auth/registration.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width, initial-scale-1.0">
+        <meta http-equiv="x-UA-Compatible" content="ie*edge">
+        <title>Register</title>
+        <link rel="stylesheet" href="<?= base_url('/bootstrap/css/bootstrap.min.css');?>">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row" style="margin-top:45px">
+                <div class="col-md-4 col-md-offset-4">
+                    <h4>Sign Up</h4><hr>
+                    <form action="" >
+                        <div class="form-group">
+                            <label for="">Name:</label>
+                            <input type="text" class="form-control" name="txtname" placeholder="Enter Your Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Lastname:</label>
+                            <input type="text" class="form-control" name="txtlastname" placeholder="Enter Your Lastname">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Email:</label>
+                            <input type="text" class="form-control" name="txtemail" placeholder="Enter Your Email">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Password:</label>
+                            <input type="text" class="form-control" name="txtpasswd" placeholder="Enter Your Password">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Retry-Password:</label>
+                            <input type="text" class="form-control" name="txtcpasswd" placeholder="Retry-Password">
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-primary btn-block" type="submit">Sign up</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
